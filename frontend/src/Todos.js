1b0ef48fe2aea9661328a088a4298c9ab17b9e27
Thulasiram Valleru <thulasiram.valleru@gmail.com>
2025-06-25 23:55:51 +0530

    modify the backend url

--- frontend/src/Todos.js
+++ frontend/src/Todos.js
@@ -19,7 +19,7 @@ export default function Todos() {
   const [error, setError] = useState(null);
   const [showStatus, setShowStatus] = useState(null);
 
-  const baseUrl = "http://localhost:8000";
+  const baseUrl = "http://todo-backend.backend.local:8000";
   function getTasks() {
     console.log("Get Task List")    
     axios({
