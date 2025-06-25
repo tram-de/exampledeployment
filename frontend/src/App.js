e23d21e21027d2db1e13821530d758c546c387b4
Thulasiram Valleru <thulasiram.valleru@gmail.com>
2025-06-25 08:30:49 +0530

    comment to frontend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 /**
- * Main application component
+ * Main application component for frontend
+ * This component serves as the entry point for the React application.
  */
 
 import Todos from './Todos' 
